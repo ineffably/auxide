9e4e7c5e2c33a407b74a70248a56766aca6ed0b1
Chad Neff <ccneff@gmail.com>
2020-11-06 19:25:06 -0800

    interpolating position is working

--- client/world.ts
+++ client/world.ts
@@ -34,14 +34,19 @@ const createWorld = (loader: PIXI.Loader): CreateWorld => {
   const { spritesheet } = loader.resources["topdown"];
   if (spritesheet) {
     const { textures } = spritesheet;
-    const box1 = new PIXI.Sprite(textures['tile_205.png']);
-    const box2 = new PIXI.Sprite(textures['tile_131.png']);
-    box1.name = 'box1';
-    box2.name = 'box2';
-    stage.addChild(box1);
-    stage.addChild(box2);
-    sprites.push(box1);
-    sprites.push(box2);
+    const addSprite = (name, tile) => {
+      const sprite = new PIXI.Sprite(textures[tile]);
+      sprite.anchor.set(0.5);
+      if(sprite){
+        sprite.name = name;
+      }
+      stage.addChild(sprite);
+      sprites.push(sprite);
+      return sprite;
+    }
+    addSprite('box1', 'tile_205.png');
+    addSprite('box2', 'tile_205.png');
+    addSprite('box3', 'tile_205.png');
   }
   const world = new p2.World({
     gravity: [0, -10]
@@ -50,8 +55,7 @@ const createWorld = (loader: PIXI.Loader): CreateWorld => {
   
   const boxBody = new p2.Body({
     mass: 1,
-    position: [-1, 2],
-    fixedRotation: true
+    position: [-2, 10]
   });
   boxBody.sprite = sprites[0];
   boxBody.addShape(new p2.Box({ width: pixelsToMeters(64), height: pixelsToMeters(64) }));
@@ -59,21 +63,26 @@ const createWorld = (loader: PIXI.Loader): CreateWorld => {
   
   const boxBody2 = new p2.Body({
     mass: 1,
-    position: [-0.3, 0],
-    fixedRotation: true
+    position: [1, 6]
   });
   boxBody2.sprite = sprites[1];
   boxBody2.addShape(new p2.Box({ width: pixelsToMeters(64), height: pixelsToMeters(64) }));
   world.addBody(boxBody2);
   
-  bodies['box1'] = boxBody;
-  bodies['box2'] = boxBody2;
 
   // Create ground
   const planeShape = new p2.Plane();
-  const plane = new p2.Body({ position: [0, -1], });
+  const plane = new p2.Body({ position: [0, -10], });
   plane.addShape(planeShape);
   world.addBody(plane);
+
+  // const box3 = new p2.Body({ mass:1, position:[0, 12], angularVelocity:1 });
+  // box3.addShape(new p2.Box({ width: 2, height: 1 }))
+  // world.addBody(box3);
+
+  bodies['box1'] = boxBody;
+  bodies['box2'] = boxBody2;
+  // bodies['box3'] = box3;
 
   console.log(world);
   return {
@@ -86,16 +95,20 @@ const updateWorld = (state: GameState): void => {
   const setSpritePosition = (sprite: PIXI.Sprite, body: p2.Body) => {
     // sprite.position.x = body.interpolatedPosition[0];
     // sprite.position.y = body.interpolatedPosition[1];
-    // sprite.rotation = body.interpolatedAngle;
     // console.log('position', sprite.position);
-    sprite.position.x = -metersToPixels(body.interpolatedPosition[0])
-    sprite.position.y = -metersToPixels(body.interpolatedPosition[1])
+    sprite.position.x = -metersToPixels(body.interpolatedPosition[0]) + 400;
+    sprite.position.y = -metersToPixels(body.interpolatedPosition[1]) + 200;
+    sprite.rotation = body.interpolatedAngle;
   }
   const box1 = state.sprites.find(sprite => sprite.name === 'box1');
   const box2 = state.sprites.find(sprite => sprite.name === 'box2');
+  // const box3 = state.sprites.find(sprite => sprite.name === 'box3');
   setSpritePosition(box1, bodies['box1']);
   setSpritePosition(box2, bodies['box2']);
+  // setSpritePosition(box3, bodies['box3']);
 }
+
+console.log(-metersToPixels(400));
 
 export { 
   createWorld,
